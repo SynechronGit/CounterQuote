fe73fe0b019ef07359d546c9e32160d9d6b4c7c6
Kalpesh <kalpesh.mahajan1@synechron.com>
2017-09-21 15:40:58 +0530

    Code clean

---
+++ SyneScanner/SyneScanner/Data Adapter/APIURL.swift
@@ -0,0 +1,13 @@
+//
+//  APIURL.swift
+//  SyneScanner
+//
+//  Created by Markel on 21/09/17.
+//  Copyright © 2017 Kartik. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "https://ocr-api.azurewebsites.net/api/"
+let UPLOAD_FILE = "CommonBlob/UploadFile"
+let START_WORKFLOW = "DocumentExtractor/StartWorkflow"
