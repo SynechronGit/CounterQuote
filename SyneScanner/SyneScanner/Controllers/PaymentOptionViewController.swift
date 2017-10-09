3fede593c2e1f8e7cfed4fffb4248acf9781f035
Kartik Sivakumar <Kartik.Sivakumar@synechron.com>
2017-10-09 20:30:07 +0530

    Updated animations on Payment options screen

--- SyneScanner/SyneScanner/Controllers/PaymentOptionViewController.swift
+++ SyneScanner/SyneScanner/Controllers/PaymentOptionViewController.swift
@@ -22,6 +22,10 @@ class PaymentOptionViewController: UIViewController {
     @IBOutlet weak var innerViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var cardViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var invoiceViewHeightConstriant: NSLayoutConstraint!
+    
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var invoiceView: UIView!
+    @IBOutlet weak var backBtnViewHeightConstraint: NSLayoutConstraint!
     var companyDetails:[String:String]?
     var addCardVC : AddCardViewController?
     var invoiceVC : InvoicePaymentViewController?
@@ -35,7 +39,7 @@ class PaymentOptionViewController: UIViewController {
         self.addChildViewController(addCardVC!)
         self.addCardVC?.companyDetails = self.companyDetails
         self.invoicePayScrollView.contentSize = CGSize(width: (self.invoiceVC?.view.frame.size.width)!, height: 200)
-        self.cardPayScrollView.contentSize = CGSize(width: (self.addCardVC?.view.frame.size.width)!, height: 568)
+        self.cardPayScrollView.contentSize = CGSize(width: (self.addCardVC?.view.frame.size.width)!, height: UIScreen.main.bounds.height)
         
         isCardViewShown = false
         isInvoiceViewShown = false
@@ -50,16 +54,16 @@ class PaymentOptionViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        cardPayButton.layer.borderWidth = 1
-        cardPayButton.layer.cornerRadius = 22
-        cardPayButton.layer.borderColor = UIColor(red: 53/255, green: 28/255, blue: 71/255, alpha: 1).cgColor
-        invoicePayButton.layer.borderWidth = 1
-        invoicePayButton.layer.cornerRadius = 22
-        invoicePayButton.layer.borderColor = UIColor(red: 53/255, green: 28/255, blue: 71/255, alpha: 1).cgColor
+//        cardPayButton.layer.borderWidth = 1
+//        cardPayButton.layer.cornerRadius = 22
+//        cardPayButton.layer.borderColor = UIColor(red: 53/255, green: 28/255, blue: 71/255, alpha: 1).cgColor
+//        invoicePayButton.layer.borderWidth = 1
+//        invoicePayButton.layer.cornerRadius = 22
+//        invoicePayButton.layer.borderColor = UIColor(red: 53/255, green: 28/255, blue: 71/255, alpha: 1).cgColor
         
         innerView.layer.cornerRadius = 12
         innerView.layer.borderWidth = 1
-        innerView.layer.borderColor = UIColor(red: 226/255, green: 226/255, blue: 226/255, alpha: 1).cgColor
+        innerView.layer.borderColor = UIColor(red: 53/255, green: 28/255, blue: 71/255, alpha: 1).cgColor
     }
 
     override func didReceiveMemoryWarning() {
@@ -78,127 +82,117 @@ class PaymentOptionViewController: UIViewController {
     //MARK: UIButton action methods
     
     @IBAction func cardPayTapped(_ sender: Any) {
-//        SVProgressHUD.show()
-//        SVProgressHUD.dismiss(withDelay: 1) {
-//            self.performSegue(withIdentifier: "NavToPayment", sender: nil)
-//        }
-        if isCardViewShown {
-            invoiceViewHeightConstriant.constant = 70
-            
-            UIView.animate(withDuration: 1.5, delay: 0.5,
-                           usingSpringWithDamping: 0.5,
-                           initialSpringVelocity: 0.8,
-                           options: [], animations: {
-                            self.innerViewLeadingConstraint.constant = 20
-                            self.innerViewTrailingConstraint.constant = 20
-                            self.innerViewHeightConstraint.constant = 140
-                            self.cardViewHeightConstraint.constant = 70
-                            self.invoiceViewHeightConstriant.constant = 70
-                            self.addCardVC?.view.removeFromSuperview()
-                            self.invoiceVC?.view.removeFromSuperview()
-                            self.innerView.layoutIfNeeded()
-            }, completion: nil)
-            isCardViewShown = false
-        } else {
-            if isInvoiceViewShown {
-                UIView.animate(withDuration: 1.5, delay: 0.5,
-                               usingSpringWithDamping: 0.5,
-                               initialSpringVelocity: 0.8,
-                               options: [], animations: {
-                                self.innerViewLeadingConstraint.constant = 20
-                                self.innerViewTrailingConstraint.constant = 20
-                                self.innerViewHeightConstraint.constant = 140
-                                self.invoiceViewHeightConstriant.constant = 70
-                                self.cardViewHeightConstraint.constant = 70
-                                self.innerView.layoutIfNeeded()
-                                self.invoiceVC?.view.removeFromSuperview()
-                                self.addCardVC?.view.removeFromSuperview()
-                                self.invoicePayScrollView.layoutIfNeeded()
-                }, completion: nil)
-                isInvoiceViewShown = false
-            }
-            invoiceViewHeightConstriant.constant = 70
+            innerView.layer.cornerRadius = 0
             self.cardPayScrollView.bounds = CGRect.zero
-            self.cardPayScrollView.addSubview((self.addCardVC?.view)!)
-            self.addCardVC?.view.frame = CGRect(x: 0, y: 0, width: self.cardPayScrollView.frame.size.width, height: 250)
-            UIView.animate(withDuration: 1.5, delay: 0.5,
-                           usingSpringWithDamping: 0.5,
-                           initialSpringVelocity: 0.8,
-                           options: [], animations: {
+            self.addCardVC?.view.frame = CGRect(x: 0, y: UIScreen.main.bounds.height, width: self.cardPayScrollView.frame.size.width, height: UIScreen.main.bounds.height)
+//            UIView.animate(withDuration: 0.5, delay: 0,
+//                           usingSpringWithDamping: 0.5,
+//                           initialSpringVelocity: 0.8,
+//                           options: [], animations: {
+                            self.invoiceViewHeightConstriant.constant = 0
                             self.innerViewLeadingConstraint.constant = 0
                             self.innerViewTrailingConstraint.constant = 0
-                            self.innerViewHeightConstraint.constant = UIScreen.main.bounds.height - self.backBtnView.frame.size.height * 2
+                            self.innerViewHeightConstraint.constant = UIScreen.main.bounds.height
+                            self.cardViewHeightConstraint.constant = self.innerViewHeightConstraint.constant + 60
                             self.innerView.layoutIfNeeded()
-                            self.invoiceVC?.view.removeFromSuperview()
-                            self.cardViewHeightConstraint.constant = self.innerViewHeightConstraint.constant - self.invoiceViewHeightConstriant.constant
-                            self.addCardVC?.view.layoutIfNeeded()
-                            self.cardPayScrollView.layoutIfNeeded()
-                            self.innerView.layoutIfNeeded()
-            }, completion: nil)
+                            
+//            }) { _ in
+                UIView.animate(withDuration: 1.0, delay: 0,
+                               usingSpringWithDamping: 0.8,
+                               initialSpringVelocity: 0.5,
+                               options: [], animations: {
+                                self.addCardVC?.view.frame = CGRect(x: 0, y: 0, width: self.cardPayScrollView.frame.size.width, height: UIScreen.main.bounds.height - 60)
+                                }, completion: nil)
+//            }
+            self.cardPayButton.isHidden = true
+            self.cardPayScrollView.addSubview((self.addCardVC?.view)!)
             isCardViewShown = true
-        }
+//        }
     }
     
     @IBAction func invoicePayTapped(_ sender: Any) {
-//        SVProgressHUD.show()
-//        SVProgressHUD.dismiss(withDelay: 1) {
-//            self.performSegue(withIdentifier: "InvoiceToReceipt", sender: nil)
-//        }
-        if isInvoiceViewShown {
-            UIView.animate(withDuration: 1.5, delay: 0.5,
-                           usingSpringWithDamping: 0.5,
-                           initialSpringVelocity: 0.8,
-                           options: [], animations: {
-                            self.innerViewLeadingConstraint.constant = 20
-                            self.innerViewTrailingConstraint.constant = 20
-                            self.innerViewHeightConstraint.constant = 140
-                            self.invoiceViewHeightConstriant.constant = 70
-                            self.cardViewHeightConstraint.constant = 70
-                            self.innerView.layoutIfNeeded()
-                            self.invoiceVC?.view.removeFromSuperview()
-                            self.addCardVC?.view.removeFromSuperview()
-                            self.invoicePayScrollView.layoutIfNeeded()
-            }, completion: nil)
-            isInvoiceViewShown = false
-        } else {
-            if isCardViewShown {
-                UIView.animate(withDuration: 1.5, delay: 0.5,
-                               usingSpringWithDamping: 0.5,
-                               initialSpringVelocity: 0.8,
-                               options: [], animations: {
-                                self.innerViewLeadingConstraint.constant = 20
-                                self.innerViewTrailingConstraint.constant = 20
-                                self.innerViewHeightConstraint.constant = 140
-                                self.cardViewHeightConstraint.constant = 70
-                                self.invoiceViewHeightConstriant.constant = 70
-                                self.addCardVC?.view.removeFromSuperview()
-                                self.invoiceVC?.view.removeFromSuperview()
-                                self.innerView.layoutIfNeeded()
-                }, completion: nil)
-                isCardViewShown = false
-            }
-            cardViewHeightConstraint.constant = 70
-            self.invoiceVC?.view.frame = CGRect(x: 0, y: 0, width: self.invoicePayScrollView.frame.size.width, height: 568)
-            self.invoicePayScrollView.addSubview((self.invoiceVC?.view)!)
-            UIView.animate(withDuration: 1.5, delay: 0.5,
-                           usingSpringWithDamping: 0.5,
-                           initialSpringVelocity: 0.8,
-                           options: [], animations: {
+            innerView.layer.cornerRadius = 0
+            self.invoicePayScrollView.bounds = CGRect.zero
+            self.invoiceVC?.view.frame = CGRect(x: 0, y: -UIScreen.main.bounds.height, width: self.invoicePayScrollView.frame.size.width, height: 568)
+//            UIView.animate(withDuration: 0.5, delay: 0,
+//                           usingSpringWithDamping: 0.5,
+//                           initialSpringVelocity: 0.8,
+//                           options: [], animations: {
+                            self.cardViewHeightConstraint.constant = 0
                             self.innerViewLeadingConstraint.constant = 0
                             self.innerViewTrailingConstraint.constant = 0
-                            self.innerViewHeightConstraint.constant = UIScreen.main.bounds.height - self.backBtnView.frame.size.height * 2
-                            self.innerView.layoutIfNeeded()
-                            self.addCardVC?.view.removeFromSuperview()
-                            self.invoiceViewHeightConstriant.constant = self.innerViewHeightConstraint.constant - self.cardViewHeightConstraint.constant
-                            self.invoicePayScrollView.layoutIfNeeded()
-                            self.innerView.layoutIfNeeded()
-            }, completion: nil)
+                            self.innerViewHeightConstraint.constant = UIScreen.main.bounds.height + 60
+                            self.invoiceViewHeightConstriant.constant = self.innerViewHeightConstraint.constant
+                            
+//            }) { _ in
+                UIView.animate(withDuration: 1.0, delay: 0,
+                               usingSpringWithDamping: 0.8,
+                               initialSpringVelocity: 0.5,
+                               options: [], animations: {
+                                self.invoiceVC?.view.frame = CGRect(x: 0, y: 0, width: self.invoicePayScrollView.frame.size.width, height: 568)
+                }, completion: nil)
+//            }
+            self.invoicePayButton.isHidden = true
+            self.invoicePayScrollView.addSubview((self.invoiceVC?.view)!)
             isInvoiceViewShown = true
-        }
+//        }
     }
     
     @IBAction func popToRoot() {
-        self.navigationController?.popViewController(animated: true)
+        innerView.layer.cornerRadius = 12
+        if isCardViewShown {
+            invoiceViewHeightConstriant.constant = 70
+            UIView.animate(withDuration: 1.0, delay: 0.1,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 0.8,
+                           options: [], animations: {
+                            self.addCardVC?.view.frame = CGRect(x: 0, y: UIScreen.main.bounds.height, width: self.cardPayScrollView.frame.size.width, height: UIScreen.main.bounds.height)
+                            
+            }) { _ in
+                UIView.animate(withDuration: 1.0, delay: 0.1,
+                               usingSpringWithDamping: 0.8,
+                               initialSpringVelocity: 0.5,
+                               options: [], animations: {
+                                self.cardViewHeightConstraint.constant = 70
+                                self.invoiceViewHeightConstriant.constant = 70
+                                self.innerViewHeightConstraint.constant = 140
+                                self.innerViewLeadingConstraint.constant = 20
+                                self.innerViewTrailingConstraint.constant = 20
+                                self.innerView.layoutIfNeeded()
+                }, completion: nil)
+            }
+            self.cardPayButton.isHidden = false
+            self.addCardVC?.view.removeFromSuperview()
+            self.invoiceVC?.view.removeFromSuperview()
+            isCardViewShown = false
+
+        } else if isInvoiceViewShown {
+            UIView.animate(withDuration: 1.0, delay: 0.1,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 0.8,
+                           options: [], animations: {
+                            self.invoiceVC?.view.frame = CGRect(x: 0, y: -UIScreen.main.bounds.height, width: self.invoicePayScrollView.frame.size.width, height: 568)
+            }) { _ in
+                UIView.animate(withDuration: 1.0, delay: 0.1,
+                               usingSpringWithDamping: 0.8,
+                               initialSpringVelocity: 0.5,
+                               options: [], animations: {
+                                self.invoiceViewHeightConstriant.constant = 70
+                                self.cardViewHeightConstraint.constant = 70
+                                self.innerViewHeightConstraint.constant = 140
+                                self.innerViewLeadingConstraint.constant = 20
+                                self.innerViewTrailingConstraint.constant = 20
+                                self.innerView.layoutIfNeeded()
+                }, completion: nil)
+            }
+            self.invoicePayButton.isHidden = false
+            self.invoiceVC?.view.removeFromSuperview()
+            self.addCardVC?.view.removeFromSuperview()
+            isInvoiceViewShown = false
+
+        } else {
+            self.navigationController?.popViewController(animated: true)
+        }
     }
 
 }
