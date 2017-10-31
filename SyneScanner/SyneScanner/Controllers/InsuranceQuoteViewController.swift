7ce79ab0ff2cc474a25bb1b0702bc455bb74213a
Kalpesh <kalpesh.mahajan1@synechron.com>
2017-10-31 13:38:03 +0530

    Pod removed

--- SyneScanner/SyneScanner/Controllers/InsuranceQuoteViewController.swift
+++ SyneScanner/SyneScanner/Controllers/InsuranceQuoteViewController.swift
@@ -106,6 +106,7 @@ class InsuranceQuoteViewController: BaseViewController {
         SVProgressHUD.dismiss(withDelay: 1) {
             self.performSegue(withIdentifier: "navToPaymentScreen", sender: nil)
         }
+        //a
     }
     
     
